5b4b42ca901be94703bc2a526911e5ca67c27a77
enzoesper <enzoesper@hotmail.com>
2019-09-29 17:45:35 -0300

    simple web scraper to imbd (internet movie data base) page obtaining the title and the rating of a certain movie

---
+++ index.js
@@ -0,0 +1,14 @@
+const request = require("request-promise");
+const cheerio = require("cheerio");
+
+const URL = "https://www.imdb.com/title/tt1285016/?ref_=nv_sr_1?ref_=nv_sr_1";
+
+(async () => {
+  const response = await request(URL);
+
+  let $ = cheerio.load(response);
+
+  let title = $('div[class="title_wrapper"] > h1').text();
+  let rating = $('span[itemprop="ratingValue"]').text();
+  console.log(title, rating);
+})();
